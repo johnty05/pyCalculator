957676bedd49ff368fc6c4c18abc53d9ae17e3b2
Saurav Panigrahi <saurav.panigrahi2016@vitstudent.ac.in>
2018-05-23 08:36:36 +0530

    Additional functions and classes have been updated

--- pycalc.py
+++ pycalc.py
@@ -1,4 +1,5 @@
 # using of tkinter for GUI in Python Programming
+
 from tkinter import *
 import math
 
@@ -17,4 +18,26 @@ class Calculator():
     # We defined a class Calculator and linked the attributes with the object.
     
     def number_press(self,num):
+        self.eq=False
+        tmp=text_box.get()
+        tmp2=str(num)
+        if self.newNumber=True:
+            self.curr=tmp2
+            self.newNumber=False
+        else:
+            if tmp2=='.':
+                if tmp2 in tmp:
+                    return
+            self.curr=tmp+tmp2
+        self.display(self.curr)
+        
+    def calTotal(self):
+        self.eq = True
+        self.curr = float(self.curr)
+        # We convert the data type to float
+        if self.operator_pend == True:
+            self.doSum()
+        else:
+            self.total = float(text_box.get())
+    
         
